4b1bf359b7f9928341783581cd34de52d43643a1
Cedric Boverie <ced@boverie.eu>
2012-02-29 22:30:03 +0100

    Working Twitter with multi-users support

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -8,17 +8,21 @@
 
 import re
 from plugin import *
+import ConfigParser
 
 try:
    import tweepy
 except ImportError:
    raise NecessaryModuleNotFound("Tweepy library not found. Please install Tweepy library! e.g. sudo easy_install tweepy")
 
+try:
+    config = ConfigParser.RawConfigParser()
+    config.read("plugins/twitter.conf")
 
-consumer_key="oOUrrc7UYBLoRjSLBIQLQ"
-consumer_secret="nA5Ta9zqdN5KFbGPCZuu2CwLHH8ou43fO2gN7e83w"
-access_token="507859408-sdXybfdtJTKckfJLTJVNMlTwzQ8YJZr8ObDgEYU9"
-access_token_secret="HWBecAnuDTM8Lq2MEOye3H0ksUc9rSiGm5YbcW9VGBk"
+    consumer_key = config.get("consumer","consumer_key")
+    consumer_secret = config.get("consumer","consumer_secret")
+except:
+    raise ApiKeyNotFoundException("You need to configure your twitter consumer_key and consumer secret to use Twitter plugin.")
 
 class Twitter(Plugin):
 
@@ -47,6 +51,10 @@ class Twitter(Plugin):
             'en-US': u"Something doesn't work as expected. Try again later.",
             'fr-FR': u"Quelque chose s'est mal passé. Veuillez réessayer plus tard.",
         },
+        'not_ready': {
+            'en-US': u"You must configure your Twitter account. Here is the identifant needed by the server administrator : {0}",
+            'fr-FR': u"Votre compte Twitter n'est pas configuré. Voici l'identifiant que vous devez transmettre à l'administrateur du serveur : {0}",
+        },
     }
 
     @register("fr-FR", res["twitter"]["fr-FR"])
@@ -61,13 +69,24 @@ class Twitter(Plugin):
         if twitterMsg == "":
             twitterMsg = self. ask(self.res["what_to_tweet"][language],self.res["what_to_tweet_say"][language])
 
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
         try:
-            api.update_status(twitterMsg)
-            self.say(self.res["success"][language].format(twitterMsg),self.res["success_say"][language].format(twitterMsg))
+            access_token = config.get(self.assistant.accountIdentifier,"access_token")
+            access_token_secret = config.get(self.assistant.accountIdentifier,"access_token_secret")
         except:
-            self.say(self.res["failure"][language])
+            access_token = ""
+            access_token_secret = ""
+
+
+        if access_token != "" and access_token_secret != "":
+            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+            api = tweepy.API(auth)
+            try:
+                api.update_status(twitterMsg)
+                self.say(self.res["success"][language].format(twitterMsg),self.res["success_say"][language].format(twitterMsg))
+            except:
+                self.say(self.res["failure"][language])
+        else:
+            self.say(self.res["not_ready"][language].format(self.assistant.accountIdentifier))
 
         self.complete_request()
