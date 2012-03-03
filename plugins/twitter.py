cb1bd07dc6a5cfd0b2f69ef81204963163ca2c59
Cedric Boverie <ced@boverie.eu>
2012-03-03 02:02:57 +0100

    New version of Twitter plugin (with auto-registration)

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Twitter
+# Post to Twitter
 # Par Cédric Boverie (cedbv)
 
-# For now, use the same account for everyone.
-# Pre-configured with a test account
+# This plugin requires a web server (like Apache) with SiriServer-WebAddons (a php script) installed.
+# https://github.com/cedbv/SiriServer-WebAddons
+# TODO: Replace twitter.conf by db
+# TODO: documentation
 
 import re
 from plugin import *
@@ -15,14 +17,8 @@ try:
 except ImportError:
    raise NecessaryModuleNotFound("Tweepy library not found. Please install Tweepy library! e.g. sudo easy_install tweepy")
 
-try:
-    config = ConfigParser.RawConfigParser()
-    config.read("plugins/twitter.conf")
-
-    consumer_key = config.get("consumer","consumer_key")
-    consumer_secret = config.get("consumer","consumer_secret")
-except:
-    raise ApiKeyNotFoundException("You need to configure your twitter consumer_key and consumer secret to use Twitter plugin.")
+twitterConfigFile = APIKeyForAPI("webaddons_path")+"/twitter.conf"
+webaddons_url = APIKeyForAPI("webaddons_url")
 
 class Twitter(Plugin):
 
@@ -53,7 +49,7 @@ class Twitter(Plugin):
         },
         'not_ready': {
             'en-US': u"You must configure your Twitter account. Here is the identifant needed by the server administrator : {0}",
-            'fr-FR': u"Votre compte Twitter n'est pas configuré. Voici l'identifiant que vous devez transmettre à l'administrateur du serveur : {0}",
+            'fr-FR': u"Votre compte n'est pas configuré. Connectez-vous avec le bouton ci-dessous pour que je puisse envoyer des tweets pour vous :",
         },
     }
 
@@ -66,18 +62,21 @@ class Twitter(Plugin):
         else:
             twitterMsg = ""
 
-        if twitterMsg == "":
-            twitterMsg = self. ask(self.res["what_to_tweet"][language],self.res["what_to_tweet_say"][language])
-
+        config = ConfigParser.RawConfigParser()
+        config.read(twitterConfigFile)
         try:
+            consumer_key = config.get("consumer","consumer_key")
+            consumer_secret = config.get("consumer","consumer_secret")
             access_token = config.get(self.assistant.accountIdentifier,"access_token")
             access_token_secret = config.get(self.assistant.accountIdentifier,"access_token_secret")
         except:
             access_token = ""
             access_token_secret = ""
 
-
         if access_token != "" and access_token_secret != "":
+            if twitterMsg == "":
+                twitterMsg = self. ask(self.res["what_to_tweet"][language],self.res["what_to_tweet_say"][language])
+
             auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
             api = tweepy.API(auth)
@@ -87,6 +86,9 @@ class Twitter(Plugin):
             except:
                 self.say(self.res["failure"][language])
         else:
-            self.say(self.res["not_ready"][language].format(self.assistant.accountIdentifier))
+            self.say(self.res["not_ready"][language])
+            url = webaddons_url+"/twitter.php?id=" + self.assistant.accountIdentifier
+            button = Button(text=u"Connectez-vous sur Twitter", commands=[OpenLink(ref=url.replace("//",""))])
+            self.send_object(AddViews(self.refId, views=[button]))
 
         self.complete_request()
